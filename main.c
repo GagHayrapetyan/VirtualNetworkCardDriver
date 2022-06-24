93f5a549bddc1902184dcae538cabc3fd203e699
gag <gagik.nh@grovf.com>
2022-06-24 13:14:16 +0400

    rename

--- main.c
+++ main.c
@@ -17,26 +17,24 @@
 #include <asm/checksum.h>
 
 
-struct snull_priv {
-    spinlock_t lock;
-};
+struct net_device *drv_dev = NULL;
 
-static int snull_open(struct net_device *dev)
+
+static int drv_open(struct net_device *dev)
 {
-    
-    printk(KERN_INFO "SNULL open...\n");
+    printk(KERN_INFO "Driver open...\n");
     netif_start_queue(dev);
     return 0;
 }
 
-static int snull_release(struct net_device *dev)
+static int drv_release(struct net_device *dev)
 {
-    printk(KERN_INFO "SNULL release...\n");
+    printk(KERN_INFO "Driver release...\n");
     netif_stop_queue(dev);
     return 0;
 }
 
-static int snull_xmit(struct sk_buff *skb, struct net_device *dev)
+static int drv_xmit(struct sk_buff *skb, struct net_device *dev)
 {
     struct iphdr *ip_header = (struct iphdr *)skb_network_header(skb);
     unsigned int src_ip = (unsigned int)ip_header->saddr;
@@ -57,68 +55,58 @@ static int snull_xmit(struct sk_buff *skb, struct net_device *dev)
     netif_rx_ni(skb);
     
 
-    printk(KERN_INFO "SNULL xmit...\n");
+    printk(KERN_INFO "Driver xmit...\n");
 
     return 0;
 }
 
-static int snull_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd)
+static int drv_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd)
 {
-    printk(KERN_INFO "SNULL ioctl: command: %x\n", cmd);
+    printk(KERN_INFO "Driver ioctl: command: %x\n", cmd);
     return 0;
 }
 
 // ---------------------------------------------------------------
 
-static const struct net_device_ops snull_netdev_ops = {
-        .ndo_open = snull_open,
-        .ndo_stop = snull_release,
-        .ndo_start_xmit = snull_xmit,
-        .ndo_do_ioctl = snull_ioctl
+static const struct net_device_ops drv_netdev_ops = {
+        .ndo_open = drv_open,
+        .ndo_stop = drv_release,
+        .ndo_start_xmit = drv_xmit,
+        .ndo_do_ioctl = drv_ioctl
 };
 
 
-void snull_init(struct net_device *dev)
+void drv_init(struct net_device *dev)
 {
-    struct snull_priv *priv;
-
     ether_setup(dev);
-    dev->netdev_ops = &snull_netdev_ops;
+    dev->netdev_ops = &drv_netdev_ops;
     dev->watchdog_timeo = 5;
     dev->flags           |= IFF_NOARP;
     dev->features        |= NETIF_F_HW_CSUM;
 
-    priv = netdev_priv(dev);
-    memset(priv, 0, sizeof(struct snull_priv));
-    spin_lock_init(&priv->lock);
-
-    printk(KERN_INFO "SNULL device initialized!\n");
+    printk(KERN_INFO "Driver device initialized!\n");
 }
 
-// ---------------------------------------------------------------
-
-struct net_device *snull_dev = NULL;
-
-static int __init snull_init_module(void)
+static int __init drv_init_module(void)
 {
-    printk(KERN_INFO "SNULL init module...\n");
-    snull_dev = alloc_netdev(0, "SNULL", NET_NAME_UNKNOWN, snull_init);
-    if (register_netdev(snull_dev)) {
-        printk("SNULL: device init error\n");
+    printk(KERN_INFO "Driver init module...\n");
+    drv_dev = alloc_netdev(0, "myeth", NET_NAME_UNKNOWN, drv_init);
+    if (register_netdev(drv_dev)) {
+        printk("Driver: device init error\n");
         return 1;
     }
     return 0;
 }
 
-static void __exit snull_exit_module(void)
+static void __exit drv_exit_module(void)
 {
-    unregister_netdev(snull_dev);
-    printk(KERN_INFO "SNULL exit module... Goodbye!\n");
+    unregister_netdev(drv_dev);
+    printk(KERN_INFO "Driver exit module... Goodbye!\n");
 }
 
 
-module_init(snull_init_module);
-module_exit(snull_exit_module);
+module_init(drv_init_module);
+module_exit(drv_exit_module);
 
 
 MODULE_LICENSE("GPL");
